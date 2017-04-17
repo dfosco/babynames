7e0b3f16cacfa3aef42f0db1d7fb8204ab18a029
Daniel Fosco <danielfosco@gmail.com>
2017-04-16 22:26:37 -0300

    Add mock data and default css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import data from './data/data'
 
 ReactDOM.render(
-  <App />,
+  <App data={data} />,
   document.getElementById('root')
 );
